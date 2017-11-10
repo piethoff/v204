a28e654329e5ad97618b405dd6715f7c15ecdf16
Piet Hoffmann <hoffmannpiet42@gmail.com>
2017-11-10 18:54:44 +0100

    mv static1.py to plot.py

--- plot.py
+++ plot.py
@@ -1,21 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+t, a, b, c, d, e, f, g, h = np.genfromtxt('content/statische_messung.txt', unpack=True)
 
 plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+plt.plot(t, a, label="Temperatur 1")
+plt.plot(t, d, label="Temperatur 4")
+plt.legend()
 
 plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+plt.plot(t,e, label="Temperatur 5")
+plt.plot(t,h, label="Temperatur 8")
+plt.legend()
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.tight_layout()
+
+plt.savefig("build/static1.pdf")
