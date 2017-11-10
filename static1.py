65535f8a869c7cb9578d32094afa31ef31b3a4a1
Piet Hoffmann <hoffmannpiet42@gmail.com>
2017-11-10 18:17:40 +0100

    changed number of variables from txt in static.py

--- static1.py
+++ static1.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a, b, c, d, e, f, g, h = np.genfromtxt('content/statische_messung.txt', unpack=True)
+t, a, b, c, d, e, f, g, h = np.genfromtxt('content/statische_messung.txt', unpack=True)
 
-t= np.arange(1, 1885, 5)
 plt.subplot(1, 2, 1)
 plt.plot(t, a, label="Temperatur 1")
 plt.plot(t, d, label="Temperatur 4")
