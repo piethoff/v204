fcd9a641198bbafac09a6ea9662d7fd860643596
Piet Hoffmann <hoffmannpiet42@gmail.com>
2017-11-10 20:25:00 +0100

    fixing Makefile for different matplotlibrc and cleaning up plot.py

--- plot.py
+++ plot.py
@@ -1,18 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-t, a, b, c, d, e, f, g, h = np.genfromtxt('content/statische_messung.txt', unpack=True)
+data = np.genfromtxt('content/statische_messung.txt', unpack=True)
 
 plt.subplot(1, 2, 1)
-plt.plot(t, a, label="Temperatur 1")
-plt.plot(t, d, label="Temperatur 4")
+plt.plot(data[0], data[1], label="Temperatur 1")
+plt.plot(data[0], data[4], label="Temperatur 4")
 plt.legend()
 
 plt.subplot(1, 2, 2)
-plt.plot(t,e, label="Temperatur 5")
-plt.plot(t,h, label="Temperatur 8")
+plt.plot(data[0], data[5], label="Temperatur 5")
+plt.plot(data[0], data[8], label="Temperatur 8")
 plt.legend()
 
 plt.tight_layout()
 
 plt.savefig("build/plot.pdf")
+plt.clf()
+
+#data = np.genfromtxt('content/dynamische_messung1.txt', unpack=True)
+
+
