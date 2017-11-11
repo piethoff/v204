c95f6995f49b5b0e919ddff2f89b75b673207102
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2017-11-11 15:28:51 +0100

    added stat_plot2 and modified auswertung.tex

--- plot.py
+++ plot.py
@@ -18,6 +18,20 @@ plt.tight_layout()
 plt.savefig("build/plot.pdf")
 plt.clf()
 
+plt.subplot(1, 2, 1)
+plt.plot(data[0], data[7]-data[8], label="Edelstahl")
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(data[0], data[2]-data[1], label="Messing")
+plt.legend()
+
+plt.tight_layout()
+
+plt.savefig("build/plot2.pdf")
+plt.clf()
+
+
 data = np.genfromtxt('content/dynamische_messung1.txt', unpack=True)
 
 plt.subplot(1, 2, 1)
@@ -34,7 +48,7 @@ plt.legend()
 
 plt.tight_layout()
 
-plt.savefig("build/plot2.pdf")
+plt.savefig("build/plot3.pdf")
 plt.clf()
 
 
@@ -45,4 +59,4 @@ plt.plot(data[0], data[7], label="Temperatur 7")
 plt.plot(data[0], data[8], label="Temperatur 8")
 plt.legend()
 
-plt.savefig("build/plot3.pdf")
+plt.savefig("build/plot4.pdf")
